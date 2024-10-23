cd585a99ee4679a4d356680d926d25775a1be6dc
vinniciusrod <163295979+vinniciusrod@users.noreply.github.com>
2024-10-23 08:27:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+document.querySelector('.mudar TextoBotao').addEventListener('click', function() {
+const paragrafo document.querySelector('.descricao');
+paragrafo.textContent = 'Texto alterado!';
+});
+document.querySelector('.destacarBotao').addEventListener('click', function() {
+const titulo = document.querySelector('.titulo');
+titulo.classList.add('destaque');
+});
